cceaa63d7813e98226b5b63ba5e29dea2d0ead08
Andrew Heykamp <Andrew.Heykamp@gmail.com>
2017-02-05 19:08:37 -0600

    Added logging libraries

--- mix.exs
+++ mix.exs
@@ -3,10 +3,12 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
-     escript: escript_config,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
+     escript:         escript_config,
+     version:         "0.1.0",
+     name:            "Issues",
+     source_url:      "https://github.com/Andrewsh86/issues",
+     elixir:          "~> 1.3",
+     build_embedded:  Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
@@ -29,8 +31,10 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      httpoison: "~> 0.9",
-      poison: "~> 2.2"
+      {:httpoison,  "~> 0.9"},
+      {:poison,     "~> 2.2"},
+      {:ex_doc,     "~> 0.12"},
+      {:earmark,    "~> 1.0", override: true}
     ]
   end
   
